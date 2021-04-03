017e17c44a9419e47b0498e0a5f14cc7638a9e81
introzorn <81861980+introzorn@users.noreply.github.com>
2021-04-03 21:44:22 +0700

    mycomit

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -3,5 +3,5 @@
 <a href="{{ route('main') }}" class="headlink"> Главная</a>
 <a href="{{ route('pasteadd') }}" class="headlink">Создать Пасту</a>
 <a href="{{ route('about') }}" class="headlink">О проекте</a>
-
+gitpull
 </div>
