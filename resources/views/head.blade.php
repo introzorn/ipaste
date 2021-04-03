a2b717484d93141c585a058201ea71af1776706d
introzorn <introzorn@yandex.ru>
2021-04-03 21:54:34 +0700

    ввввв

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -3,9 +3,5 @@
 <a href="{{ route('main') }}" class="headlink"> Главная</a>
 <a href="{{ route('pasteadd') }}" class="headlink">Создать Пасту</a>
 <a href="{{ route('about') }}" class="headlink">О проекте</a>
-<<<<<<< HEAD
-новый комит
-=======
-gitpull
->>>>>>> 017e17c44a9419e47b0498e0a5f14cc7638a9e81
+
 </div>
